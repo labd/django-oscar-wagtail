7c0b79abfcc09dd3fa1c14e2d9c53556db7dc76e
Michael van Tellingen <michael@mvantellingen.nl>
2016-08-04 13:36:22 +0200

    Make the package work! :D

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ tests_require = [
 
 setup(
     name='django-oscar-wagtail',
-    version='0.0.3',
+    version='0.0.5',
     description="Integration between Django Oscar and Wagtail",
     long_description=open('README.rst').read(),
     author="Michael van Tellingen",
